38ae5e6d17d2da74c93ef971391258311a0b0e0c
Chanjin Park <inf0craw11@gmail.com>
2021-07-12 16:43:35 +0900

    css to styled-components

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -5,12 +5,23 @@ import HeaderRight from "../components/HeaderRight";
 
 import "../css/Header.css";
 
+import styled from "styled-components";
+
+const StyledHeader = styled.div`
+    height: 64px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+
+    align-items: center;
+`;
+
 function Header() {
     return (
-        <div className="header">
+        <StyledHeader className="header">
             <Logo></Logo>
             <HeaderRight></HeaderRight>
-        </div> 
+        </StyledHeader> 
 
     );
 }

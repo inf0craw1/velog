058b52eb9ba84bfde4c24556baf570303e557176
Chanjin Park <inf0craw11@gmail.com>
2021-07-12 15:54:20 +0900

    handleClick

--- src/component/HeaderRight.js
+++ src/component/HeaderRight.js
@@ -2,13 +2,16 @@ import React from "react";
 
 function HeaderRight(){
 
+    const handleClick = function (){
+        console.log("login clicked");
+    }
 
     return (
         <div className="header-right">
             <div className="search-button">
                 🔍
             </div>
-            <div className="login-button">
+            <div className="login-button" onClick={handleClick}>
                 로그인
             </div>
 

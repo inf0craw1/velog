8541c09f67a97ea3aaac1550701dae28c169ff70
dndbekfrl1 <dndbekfrl1@gmail.com>
2021-07-12 15:52:16 +0900

    creat footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./css/App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       Velog
+      <Footer></Footer>
     </div>
   );
 }

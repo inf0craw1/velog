e1c0060a2c590d729997ae56b7b41bff64b9aca6
dndbekfrl1 <dndbekfrl1@gmail.com>
2021-07-12 16:43:51 +0900

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,41 @@
+import styled from "styled-components";
+
+const FooterBlock = styled.footer`
+  border-top: 1px solid #efefef;
+  height: 400px;
+  width: 100%;
+  color: #171717;
+  font-size: 14px;
+  display: flex;
+  padding-top: 40px;
+`;
+const FooterContainer = styled.div``;
+
+const ContentsFooter = styled.div`
+  display: flex;
+  margin-bottom: 10px;
+`;
+
+const Contents = styled.div``;
+
+const TitleFooter = styled.div`
+  font-weight: 700;
+`;
+
 function Footer() {
   return (
-    <footer>
-      <div className="container_footer">
-        <div className="creator_footer">
-          <div class="title_footer">Creator </div>
-          <div>박진아 박찬진 전혜지 정서영</div>
-        </div>
-        <div className="describe_footer">
-          <div class="title_footer">Describe</div>
-          <div>velog project for react study</div>
-        </div>
-      </div>
-    </footer>
+    <FooterBlock>
+      <FooterContainer>
+        <ContentsFooter>
+          <TitleFooter>Creator </TitleFooter>
+          <Contents>박진아 박찬진 전혜지 정서영</Contents>
+        </ContentsFooter>
+        <ContentsFooter>
+          <TitleFooter>Describe</TitleFooter>
+          <Contents>velog project forfsfs react study</Contents>
+        </ContentsFooter>
+      </FooterContainer>
+    </FooterBlock>
   );
 }
 

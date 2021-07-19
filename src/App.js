a17e89a6195214ad1f0009a60994e32d59a0d888
Jung seo young <sju06071@gmail.com>
2021-07-19 16:48:21 +0900

    7/19

--- src/App.js
+++ src/App.js
@@ -3,19 +3,43 @@ import Header from "./containers/Header";
 import Body from "./components/Body";
 
 import "./css/App.css";
-
 import Footer from "./components/Footer";
+import styled from "styled-components";
+import smile from "./img/smile.jpg";
 
+const Img = styled.div`
+  height: 210px;
+  width: 350px;
+  background-image: url(${smile});
+  background-size: cover;
+  border-top-left-radius: 16px;
+  border-top-right-radius: 16px;
+`;
+const Title = styled.h1`
+  display: inline-block;
+  list-style: none;
+  margin-top: 10px;
+  margin-left: 10px;
+  font-weight: bold;
+  font-size: 20px;
+`;
+const Post = styled.div`
+  display: inline-block;
+  list-style: none;
+  margin-top: 10px;
+  margin-left: 10px;
+  color: gray;
+  font-size: 15px;
+`;
 function App() {
   return (
     <div className="App">
-      <Header>
-
-      </Header>
+      <Header></Header>
       <div className="Body">
         <Body>
-          게시글
-          <div>2021. 7. 12 </div>
+          <Img />
+          <Title> 게시글</Title>
+          <Post> 리액트 스터디 </Post>
         </Body>
       </div>
       <Footer></Footer>
--- src/components/Body.js
+++ src/components/Body.js
@@ -2,24 +2,34 @@ import React from "react";
 import styled from "styled-components";
 
 const BodyBlock = styled.div`
-  width: 512px;
-  height: 768px;
+  width: 350px;
+  height: 400px;
 
   position: relative; /* 추후 박스 하단에 추가 버튼을 위치시키기 위한 설정 */
   background: white;
   border-radius: 16px;
   box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.04);
 
-  margin: 0 auto; /* 페이지 중앙에 나타나도록 설정 */
+  margin: 0; /* 페이지 중앙에 나타나도록 설정 */
 
   margin-top: 96px;
   margin-bottom: 32px;
   display: flex;
   flex-direction: column;
 `;
-
+const Border = styled.div`
+  margin-top: 80px;
+  border-top: 1px solid #efefef;
+`;
 function Body({ children }) {
-  return <BodyBlock>{children}</BodyBlock>;
+  return (
+    <div style={{ display: "flex" }}>
+      <BodyBlock>
+        {children}
+        <Border />
+      </BodyBlock>
+    </div>
+  );
 }
 
 export default Body;

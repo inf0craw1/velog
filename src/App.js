e3be9be1dd148285febc975ecf22fdce821adba3
Jung seo young <sju06071@gmail.com>
2021-07-28 21:23:13 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import "./containers/Header";
-import Header from "./containers/Header";
 
 import "./css/App.css";
 import Footer from "./components/Footer";
@@ -9,7 +7,6 @@ import Body from "./components/Body";
 function App() {
   return (
     <div className="App">
-      <Header></Header>
       <Body />
       <Footer></Footer>
     </div>

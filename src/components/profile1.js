98a80320a19c90a13be4fbbba0bbffc4592d15dc
Hyeji <heji0826@naver.com>
2021-08-02 05:11:25 +0900

    link 후 흰화면

--- src/components/profile1.js
+++ src/components/profile1.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router';
+import './Profile.css';
 
-const profile1 = () => {
+const MenuItem = ({active, children, to}) => (
+    <Link to={to} className={`menu-item ${active ? 'active': ''}`}>
+            {children}
+    </Link>
+);
+
+const profile1 = (props, context) => {
+    const { router } = context;
     return (
         <div>
-            <h2>Profile1</h2>
+            <div className="logo">
+                velog
+            </div>
+            <div className="menu">
+                <MenuItem to={'/'} active={router.isActive('/', true)}>글</MenuItem>
+                <MenuItem to={'/about'} active={router.isActive('/about')}>시리즈</MenuItem>
+                <MenuItem to={'/post'} active={router.isActive('/post')}>소개</MenuItem>
+            </div>
         </div>
     );
 };
+
+Header.contextTypes = {
+    router: React.PropTypes.object
+}
 
 export default profile1;

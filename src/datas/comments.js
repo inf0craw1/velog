a009a01c72824d127b83c4aa1e42876d079e9786
Chanjin Park <inf0craw11@gmail.com>
2021-08-23 15:49:12 +0900

    fetch: add search, add datas, edit footer

--- src/datas/comments.js
+++ src/datas/comments.js
@@ -46,5 +46,52 @@ export const comments = [
         date: "1일전",
       },
     ],
+  },{
+    postId: 3,
+    commentData: [
+      {
+        user: {
+          profile: "",
+          id: "jina",
+        },
+        text: "우와 대박~",
+        date: "2일전",
+      },
+      {
+        user: {
+          profile: "",
+          id: "banana",
+        },
+        text: "와우!",
+        date: "1일전",
+      },
+    ],
+  },
+
+  {
+    postId: 4,
+    commentData: [
+      {
+        user: {
+          profile: "",
+          id: "vrvr",
+        },
+        text: "유용해요",
+        date: "2일전",
+      },
+    ],
+  },
+  {
+    postId: 5,
+    commentData: [
+      {
+        user: {
+          profile: "",
+          id: "hamster",
+        },
+        text: "정말 재밋겠다~",
+        date: "1일전",
+      },
+    ],
   },
 ];
